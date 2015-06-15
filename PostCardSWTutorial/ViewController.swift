a523e7daad7a276b5da44cb4e3247486361c4c87
andretbm <andremuchagata@gmail.com>
2015-06-15 16:00:40 +0100

    Initial Commit

---
+++ PostCardSWTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostCardSWTutorial
+//
+//  Created by Andre Muchagata on 15/06/2015.
+//  Copyright (c) 2015 Andre Muchagata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
